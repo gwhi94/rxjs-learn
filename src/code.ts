36ed8a870ac6a757405af7941e50a4527c99bdc4
George Whiting <gwhi94@gmail.com>
2020-04-05 17:40:27 +0100

    unsubscribe

--- src/code.ts
+++ src/code.ts
@@ -1,4 +1,34 @@
-import * as Rx from "rxjs/Observable";
+import { Observable } from 'rxjs';
+
+var observable = Observable.create((observer:any) => {
+    try {
+        observer.next('Hello')
+        observer.next('World')
+        setInterval(() => {
+            observer.next('I am good')
+        }, 2000)
+    } catch(err) {
+        observer.error(err)
+    }
+}); 
+
+//arrow function in place of traditional function()
+//observer read values from observable
+var observer = observable.subscribe(
+    (x:any) => addItem(x),
+    (err:any) => addItem('error'),
+    () => addItem('completed')    
+);
+
+setTimeout(() => {
+    observer.unsubscribe()
+}, 6000)
 
 
-console.log(Rx);
+
+function addItem(val:any) {
+    var node = document.createElement("li");
+    var textnode = document.createTextNode(val);
+    node.appendChild(textnode);
+    document.getElementById("output").appendChild(node);
+}
